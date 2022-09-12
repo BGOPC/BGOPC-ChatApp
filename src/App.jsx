c14cac33c3abd0c4625683473cee796f51da7c1f
BGOPC <bb8933400@gmail.com>
2022-09-12 18:40:12 +0430

    Base Structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import "./styles.scss";
+function App() {
+  return (
+    // <Register/>
+    // <Login/>
+    <Home/>
+  );
+}
+
+export default App;
--- src/components/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return (<div>BGOPC Chat App</div>);
-}
-
-export default App;
